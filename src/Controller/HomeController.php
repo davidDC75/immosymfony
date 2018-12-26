b1e6f1e85c920c9bf18b67af68cdf2d90ec779f2
David De Castro <segphault@gmail.com>
2018-12-26 11:42:32 +0100

    Première page avec ajout de twig dans services.yaml et HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,20 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
 class HomeController {
+    
+    /**
+     * @var Twig\Environment
+     */
+    private $twig;
+
+    public function __construct($twig) {
+        $this->twig=$twig;
+    }
 
     public function index():Response {
-        return new Response('Salut les gens!');
+        return new Response($this->twig->render('pages/home.html.twig'));
     }
 }
