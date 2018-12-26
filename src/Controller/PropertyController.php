0b7107d6ed6c48100c2748c287f520bf9e32eb41
David De Castro <segphault@gmail.com>
2018-12-26 14:18:06 +0100

    Creation de l'entity Property, création de la table et ajout d'un premier enregistrement en base de données

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Property;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
@@ -12,6 +14,23 @@ class PropertyController extends AbstractController {
      * @return Response
      */
     public function index():Response {
+
+        $property=new Property();
+        $property->setTitle('Mon premier bien')
+            ->setPrice(200000)
+            ->setRooms(4)
+            ->setBedrooms(3)
+            ->setDescription('Une petite description')
+            ->setSurface(60)
+            ->setFloor(4)
+            ->setHeat(1)
+            ->setCity('Montpellier')
+            ->setAddress('15 Boulevard Gambetta')
+            ->setPostalCode('34000');
+        $em=$this->getDoctrine()->getManager(); // Get Entity Manager
+        $em->persist($property);
+        $em->flush();
+
         return $this->render('property/index.html.twig',[
             'current_menu'=> 'properties'
         ]);
